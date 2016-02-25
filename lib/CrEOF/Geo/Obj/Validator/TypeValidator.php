220932ea20d37202b4211e0a3c821f63d93a97cd
Derek J. Lambert <dlambert@dereklambert.com>
2016-02-25 13:59:04 -0600

    Add TypeValidator and simple tests.

---
+++ lib/CrEOF/Geo/Obj/Validator/TypeValidator.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Copyright (C) 2016 Derek J. Lambert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CrEOF\Geo\Obj\Validator;
+
+use CrEOF\Geo\Obj\Exception\RangeException;
+use CrEOF\Geo\Obj\Exception\UnexpectedValueException;
+
+/**
+ * Class TypeValidator
+ *
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
+ * @license http://dlambert.mit-license.org MIT
+ */
+class TypeValidator implements ValidatorInterface
+{
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @param ValidatorInterface|null $validator
+     */
+    public function __construct(ValidatorInterface $validator = null)
+    {
+        if (null !== $validator) {
+            $this->validator = $validator;
+        }
+    }
+
+    /**
+     * @param array $value
+     *
+     * @throws UnexpectedValueException
+     * @throws RangeException
+     */
+    public function validate(array $value)
+    {
+        if (! array_key_exists('type', $value)) {
+            return;
+        }
+
+        $type  = $value['type'];
+        $value = $value['value'];
+
+        if (! is_subclass_of($type, 'CrEOF\Geo\Obj\AbstractObject')) {
+            throw new UnexpectedValueException();
+        }
+    }
+
+    /**
+     * @param string $type
+     *
+     * @throws UnexpectedValueException
+     */
+    public function setType($type)
+    {
+        $t = new UnexpectedValueException();
+        $t = new UnexpectedValueException();
+        if (! is_subclass_of($type, 'CrEOF\Geo\Obj\AbstractObject')) {
+            throw new UnexpectedValueException('Unsupported type "' . $type . '"');
+        }
+
+        $this->type = $type;
+    }
+}
