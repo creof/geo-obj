049d59e520e1427039775b78634b5921737a8685
Derek J. Lambert <dlambert@dereklambert.com>
2016-03-16 11:23:48 -0500

    Move GeographyValidator tests to seperate test class

--- tests/Validator/GeographyValidatorTest.php
+++ tests/Validator/GeographyValidatorTest.php
@@ -21,112 +21,87 @@
  * SOFTWARE.
  */
 
-namespace CrEOF\Geo\Obj\Tests;
+namespace CrEOF\Geo\Obj\Tests\Validator;
 
 use CrEOF\Geo\Obj\Configuration;
 use CrEOF\Geo\Obj\Exception\RangeException;
 use CrEOF\Geo\Obj\Point;
 use CrEOF\Geo\Obj\ObjectInterface;
 use CrEOF\Geo\Obj\Validator\GeographyValidator;
-use CrEOF\Geo\Obj\Validator\DValidator;
 
 /**
- * Class PointTest
+ * GeographyValidatorTest
  *
  * @author  Derek J. Lambert <dlambert@dereklambert.com>
  * @license http://dlambert.mit-license.org MIT
  */
-class PointTest extends \PHPUnit_Framework_TestCase
+class GeographyValidatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testArrayPoint()
+    /**
+     * @backupStaticAttributes
+     *
+     * @expectedException        RangeException
+     * @expectedExceptionMessage Invalid longitude value "300", must be in range -180 to 180.
+     */
+    public function testPointGeographyValidatorLongitudeFirstBadLongitude()
     {
-        $point = new Point([0,0]);
+        $validator = new GeographyValidator();
 
-        static::assertEquals([0,0], $point->getValue());
-    }
+        $validator->setOrder(GeographyValidator::CRITERIA_LONGITUDE_FIRST);
 
-    public function testWkbPoint()
-    {
-        $wkb   = pack('H*', '01010000003D0AD7A3701D41400000000000C055C0');
-        $point = new Point($wkb);
+        Configuration::getInstance()->setValidator(ObjectInterface::T_POINT, $validator);
 
-        static::assertEquals([34.23, -87], $point->getValue());
+        new Point([300, 20]);
     }
 
     /**
+     * @backupStaticAttributes
+     *
      * @expectedException        RangeException
-     * @expectedExceptionMessage Point value count must be between 2 and 4.
+     * @expectedExceptionMessage Invalid latitude value "300", must be in range -90 to 90.
      */
-    public function testShortPoint()
+    public function testPointGeographyValidatorLongitudeFirstBadLatitude()
     {
-        new Point([0]);
+        $validator = new GeographyValidator();
+
+        $validator->setOrder(GeographyValidator::CRITERIA_LONGITUDE_FIRST);
+
+        Configuration::getInstance()->setValidator(ObjectInterface::T_POINT, $validator);
+
+        new Point([20, 300]);
     }
 
     /**
+     * @backupStaticAttributes
+     *
      * @expectedException        RangeException
-     * @expectedExceptionMessage Point value count must be between 2 and 4.
+     * @expectedExceptionMessage Invalid latitude value "300", must be in range -90 to 90.
      */
-    public function testLongPoint()
-    {
-        new Point([0,0,0,0,0]);
-    }
-
-    public function testGoodPointValidatorStacking()
+    public function testPointGeographyValidatorLatitudeFirstBadLatitude()
     {
         $validator = new GeographyValidator();
 
         $validator->setOrder(GeographyValidator::CRITERIA_LATITUDE_FIRST);
 
-        $validator = new DValidator($validator);
-
-        $validator->setSize(2);
-
         Configuration::getInstance()->setValidator(ObjectInterface::T_POINT, $validator);
 
-        $point = new Point([20, 300]);
-
-        static::assertEquals([20, 300], $point->getValue());
+        new Point([300, 20]);
     }
 
     /**
      * @backupStaticAttributes
      *
      * @expectedException        RangeException
-     * @expectedExceptionMessage Invalid size "3", size must be 2.
+     * @expectedExceptionMessage Invalid longitude value "300", must be in range -180 to 180.
      */
-    public function testBadLongPointSizeInnerValidator()
+    public function testPointGeographyValidatorLatitudeFirstBadLongitude()
     {
-        $validator = new DValidator();
-
-        $validator->setSize(2);
-
-        $validator = new GeographyValidator($validator);
+        $validator = new GeographyValidator();
 
         $validator->setOrder(GeographyValidator::CRITERIA_LATITUDE_FIRST);
 
         Configuration::getInstance()->setValidator(ObjectInterface::T_POINT, $validator);
 
-        new Point([20, 10, 30]);
-    }
-
-    /**
-     * @backupStaticAttributes
-     *
-     * @expectedException        RangeException
-     * @expectedExceptionMessage Invalid size "3", size must be 4.
-     */
-    public function testBadShortPointSizeInnerValidator()
-    {
-        $validator = new DValidator();
-
-        $validator->setSize(4);
-
-        $validator = new GeographyValidator($validator);
-
-        $validator->setOrder(GeographyValidator::CRITERIA_LATITUDE_FIRST);
-
-        Configuration::getInstance()->setValidator(ObjectInterface::T_POINT, $validator);
-
-        new Point([20, 10, 30]);
+        new Point([20, 300]);
     }
 }
