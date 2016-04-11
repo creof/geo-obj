f7145f40675f5f2c0f80accb5073bbc40d30945d
Derek J. Lambert <dlambert@dereklambert.com>
2016-04-11 07:08:12 -0500

    Add ValidatePointTrait

---
+++ lib/Validator/Value/ValidatePointTrait.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Copyright (C) 2016 Derek J. Lambert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace CrEOF\Geo\Obj\Validator\Value;
+
+use CrEOF\Geo\Obj\Configuration;
+use CrEOF\Geo\Obj\Exception\ExceptionInterface;
+use CrEOF\Geo\Obj\Exception\RangeException;
+use CrEOF\Geo\Obj\Exception\UnexpectedValueException;
+use CrEOF\Geo\Obj\Object;
+
+/**
+ * Class MultiPolygonValidator
+ *
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
+ * @license http://dlambert.mit-license.org MIT
+ */
+trait ValidatePointTrait
+{
+    protected $dimension;
+
+    /**
+     * @param mixed  $point
+     * @param string $messageType Name of object containing points used in exception messages
+     *
+     * @throws ExceptionInterface
+     */
+    protected function validatePoint($point, $messageType)
+    {
+        if (! is_array($point)) {
+            throw new UnexpectedValueException($messageType . ' value must be array of "array", "' . gettype($point) . '" found');
+        }
+
+        if (null === $this->dimension) {
+            $this->dimension = count($point);
+        }
+
+        if (count($point) !== $this->dimension) {
+            throw new RangeException('Dimension mismatch'); //TODO fix message
+        }
+
+        $point = [
+            'type' => 'point',
+            'value' => $point
+        ];
+
+        try {
+            Configuration::getInstance()->getValidators(Object::T_POINT)->validate($point);
+        } catch (ExceptionInterface $e) {
+            throw new RangeException('Bad point value in ' . $messageType . '. ' . $e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
